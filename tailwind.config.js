1cd38bf29b4255a1454f3fc50802b58b349b9ca2
Paulinojr <paulino.junior@outlook.com>
2023-02-01 12:56:00 -0300

    feat: add tailwind and started header styling

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      sunflower: "#E6D029",
+      valhalla: "#201852",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
